0694e3a123a4b026a98a43363f10cfb8ece36336
S4medi <samdelestre@gmail.com>
2024-06-26 12:00:03 +0200

    classe personne

--- sam.py
+++ sam.py
@@ -1,1 +1,17 @@
 print("jet taime vraiment c est pas une blague ")
+
+class personne:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def set(self,name,age):
+        self.name = name
+        self.age = age
